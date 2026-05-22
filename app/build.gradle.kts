c43278329c76bbfd2df00065d8a751b736ec5172
Neyza <lab151-29@Lab151-29s-iMac.local>
2026-05-22 10:38:39 +0700

    Pertemuan 9 : Terakhir

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 }
 
 dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
