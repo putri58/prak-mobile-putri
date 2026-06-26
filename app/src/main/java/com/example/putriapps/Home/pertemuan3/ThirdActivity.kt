bc53108b1e79c4bbb37a84b9bd6e4a90abc39e66
Neyza <lab151-29@Lab151-29s-iMac.local>
2026-06-26 08:19:03 +0700

    Pertemuan 14 : Reminder

--- app/src/main/java/com/example/putriapps/Home/pertemuan3/ThirdActivity.kt
+++ app/src/main/java/com/example/putriapps/Home/pertemuan3/ThirdActivity.kt
@@ -14,8 +14,11 @@ import com.example.putriapps.R
 import com.example.putriapps.databinding.ActivityThirdBinding
 import com.example.putriapps.utils.NotificationHelper
 import com.example.putriapps.utils.PermissionHelper
+import com.example.putriapps.utils.ReminderHelper
+import java.util.Calendar
 
 class ThirdActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityThirdBinding
 
     private val notificationPermissionLauncher =
@@ -30,36 +33,60 @@ class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         binding = ActivityThirdBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // Inisialisasi komponen
-//        val inputNoTujuan: EditText = findViewById(R.id.inputNoTujuan)
-//        val btnKirim: Button = findViewById(R.id.btnKirim)
 
-        binding.btnKirim.setOnClickListener {
-            //Mengambil value dari inputNama dan menampilkan di Logcat
-            val nomor = binding.inputNoTujuan.text
-            val intent = Intent(this, ThirdResultActivity::class.java)
-
-            Toast.makeText(this, "Pesan ini Berhasil Dikirim ke $nomor", Toast.LENGTH_SHORT).show()
-            NotificationHelper.showNotification(
-                this, //Jika panggil di fragment maka requireContext()
-                "Pesanan Anda",
-                "Halo $nomor, Pesanan Anda Sedang Diproses",
-                intent
-            )
-        }
         setSupportActionBar(binding.toolbar)
         supportActionBar?.apply {
             title = "Activity Fifth"
             subtitle = "Ini adalah subtitle"
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
+        }
+
+        // Inisialisasi komponen
+//        val inputNoTujuan: EditText = findViewById(R.id.inputNoTujuan)
+//        val btnKirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val nomor = binding.inputNoTujuan.text.toString()
+            val intent = Intent(this, ThirdResultActivity::class.java)
+
+//            Toast.makeText(this, "Pesan ini Berhasil Dikirim ke $nomor", Toast.LENGTH_SHORT).show()
+//            NotificationHelper.showNotification(
+//                this, //Jika panggil di fragment maka requireContext()
+//                "Pesanan Anda",
+//                "Halo $nomor, Pesanan Anda Sedang Diproses",
+//                intent
+//            )
+
+            val calendar = Calendar.getInstance().apply {
+                add(Calendar.MINUTE, 1) // Tambah 1 menit dari sekarang
+            }
+
+            ReminderHelper.setReminder(
+                context = this, //Jika panggil di fragment maka requireContext()
+                hour = calendar.get(Calendar.HOUR_OF_DAY),
+                minute = calendar.get(Calendar.MINUTE),
+                title = "Reminder 1 Menit",
+                message = "Halo $nomor, reminder ini muncul 1 menit setelah tombol ditekan",
+                targetActivity = ThirdResultActivity::class.java
+            )
+
+            Toast.makeText(
+                this,
+                "Silahkan tunggu 1 Menit untuk menerima Notifikasi...",
+                Toast.LENGTH_SHORT
+            ).show()
         }
 
         if (PermissionHelper.isNotificationPermissionRequired()) {
@@ -71,14 +98,26 @@ class ThirdActivity : AppCompatActivity() {
                 )
             }
         }
-//        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//            return when (item.itemId) {
-//                android.R.id.home -> {
-//                    onBackPressedDispatcher.onBackPressed()
-//                    true
-//                }
-//
-//                else -> super.onOptionsItemSelected(item)
-//            }
-        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+
+        //        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //            return when (item.itemId) {
+        //                android.R.id.home -> {
+        //                    onBackPressedDispatcher.onBackPressed()
+        //                    true
+        //                }
+        //
+        //                else -> super.onOptionsItemSelected(item)
+        //            }
+    }
+}
